b929698efedd6ad44a4ee7ee46fcaa70fd1be6d5
Evan Lloyd New-Schmidt <lloydns@outlook.com>
2017-07-17 17:10:24 -0400

    Prep for heroku deployment

--- webapp.py
+++ webapp.py
@@ -55,5 +55,5 @@ def no_page():
 if __name__ == '__main__':
     app.debug = True  # updates the page as the code is saved
     HOST = '0.0.0.0' if 'PORT' in os.environ else '127.0.0.1'
-    PORT = int(os.environ.get('PORT', 443))
+    PORT = int(os.environ.get('PORT', 9001))
     app.run(host='0.0.0.0', port=PORT)
